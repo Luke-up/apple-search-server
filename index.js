881db34a1896edaaa238861a8187d03220a9acfc
Luke <painelukeb@gmail.com>
2022-08-13 21:06:02 +0200

    moved all functions to index.js

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 const express = require("express");
 const app = express();
 const PORT = 5000;
-const music = require("./routes/music");
-const media = require("./routes/media");
 
-app.get("/apple", (req, res) => {
-  res.send(["You've reached apple-search-server"]);
+const endPoint = "https://itunes.apple.com/search";
+
+const fetch = (...args) =>
+  import("node-fetch").then(({ default: fetch }) => fetch(...args));
+
+app.get("/*", (req, res) => {
+  fetch(
+    endPoint +
+      "?term=" +
+      req.headers.term +
+      "&media=" +
+      req.headers.media +
+      "&attribute=" +
+      req.headers.attribute +
+      "&limit=10"
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(req.headers.term);
+      res.send(data);
+    });
 });
-
-app.use("/*", media);
 
 app.listen(PORT, () => {
   console.log("Server running on port 5000!");
